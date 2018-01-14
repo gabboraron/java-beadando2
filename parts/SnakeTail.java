3c722a1e2082c1ebcfe0d405abc6c4346e18912e
gabboraron <gabboraron@gmail.com>
2018-01-14 14:04:11 +0100

    Add files via upload

---
+++ parts/SnakeTail.java
@@ -0,0 +1,24 @@
+package snake.parts;
+
+import snake.util.*;
+import snake.Tile;
+
+public class SnakeTail implements Tile{
+	Position position;
+	
+	public  SnakeTail(Position pos){
+		this.position = pos;
+	}
+	
+	public Position getPosition(){
+		return position;
+	}
+	
+	protected void moveTo(Position pos){
+		this.position = pos;
+	}
+	
+	protected boolean isAt(Position pos){
+		return this.position.isSame(pos);
+	}
+}
