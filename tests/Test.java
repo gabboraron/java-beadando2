7dd6ac6f9533a7c9ba917169695c128d4737a83f
gabboraron <gabboraron@gmail.com>
2018-01-15 00:18:18 +0100

    Add files via upload

---
+++ tests/Test.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import org.junit.Test;
+import java.util.*;
+
+import snake.util.*;
+import snake.parts.*;
+import snake.exception.*;
+import snake.Game;
+import snake.Apple;
+
+public class GTNGameTest {
+
+}
